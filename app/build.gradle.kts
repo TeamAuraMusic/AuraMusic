33e25851aa97b287e29163fbdba0fe3d6b651d20
chila254 <franklinfinyange@gmail.com>
2026-03-05 21:25:36 +0300

    Use JSON format for Listen Together (protobuf plugin unavailable)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,6 @@ plugins {
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.protobuf)
 }
 
 android {
@@ -209,24 +208,6 @@ android {
     }
 }
 
-protobuf {
-    protoc {
-        artifact = "com.google.protobuf:protoc:${libs.versions.protobuf.get()}"
-    }
-    generateProtoTasks {
-        all().forEach { task ->
-            task.builtins {
-                create("java") {
-                    option("lite")
-                }
-                create("kotlin") {
-                    option("lite")
-                }
-            }
-        }
-    }
-}
-
 ksp {
     arg("room.schemaLocation", "$projectDir/schemas")
 }
@@ -244,10 +225,6 @@ dependencies {
     implementation(libs.guava)
     implementation(libs.coroutines.guava)
     implementation(libs.concurrent.futures)
-
-    // Protobuf for Listen Together
-    implementation(libs.protobuf.javalite)
-    implementation(libs.protobuf.kotlin.lite)
 
     implementation(libs.activity)
     implementation(libs.hilt.navigation)
