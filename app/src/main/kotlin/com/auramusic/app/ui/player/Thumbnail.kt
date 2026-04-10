ef57332f81eb06dde5f0c98b65b1f72af19e48ca
chila254 <chila254@github.com>
2026-04-10 11:03:38 +0000

    Fixed - kotlinx.coroutines.launch was still needed by the SeekRippleEffect composable at line 1178. The import was incorrectly removed during the lyrics cleanup.

--- app/src/main/kotlin/com/auramusic/app/ui/player/Thumbnail.kt
+++ app/src/main/kotlin/com/auramusic/app/ui/player/Thumbnail.kt
@@ -126,6 +126,7 @@ import com.auramusic.app.utils.rememberPreference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
