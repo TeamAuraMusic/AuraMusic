3e0e7ce05a50696c9fe46ee09e4a8accfa562918
chila254 <franklinfinyange@gmail.com>
2026-06-22 09:14:01 +0000

    fix(tv): add missing grid items import for See More LazyVerticalGrid

--- app/src/tv/kotlin/com/auramusic/app/ui/tv/TvApp.kt
+++ app/src/tv/kotlin/com/auramusic/app/ui/tv/TvApp.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.relocation.BringIntoViewRequester
--- app/src/tv/kotlin/com/auramusic/app/ui/tv/TvDetailScreens.kt
+++ app/src/tv/kotlin/com/auramusic/app/ui/tv/TvDetailScreens.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
